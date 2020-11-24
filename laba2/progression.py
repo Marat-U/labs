6f2ac375f99badd8c306c9055a1af02e346c9e90
Marat-U <71347477+Marat-U@users.noreply.github.com>
2020-11-24 23:59:41 +0300

    Update progression.py

--- laba2/progression.py
+++ laba2/progression.py
@@ -1,67 +1,57 @@
 class Progression:
-  def __init__(self, first, step):
-    self.first = first
-    self.step = step
+    def __init__(self, first, step):
+        self.first = first
+        self.step = step
 
-  def compute_n_elem(self, n):
-    pass
+    def set_strategy(self, strategy):
+        self.strategy = strategy
 
-  def sum(self, n):
-    pass
+    def compute_n_elem(self, n):
+        print(self.strategy.compute_n_elem(n))
+
+    def sum(self, n):
+        print(self.strategy.sum(n))
 
 class ArithmeticProgression(Progression):
-  def __init__(self, first, step):
-    super().__init__(first, step)
+    def __init__(self, first, step):
+        super().__init__(first, step)
 
-  def compute_n_elem(self, n):
-    a_n = self.first
-    for i in range(n - 1):
-      a_n += self.step
-    return a_n
+    def compute_n_elem(self, n):
+        a_n = self.first
+        for i in range(n - 1):
+            a_n += self.step
+        return a_n
 
-  def sum(self, n):
-    summ = 0
-    for i in range(1, n + 1):
-      a_n = self.first + self.step * (i - 1)
-      summ += a_n
-    return summ
+    def sum(self, n):
+        summ = 0
+        for i in range(1, n + 1):
+          a_n = self.first + self.step * (i - 1)
+          summ += a_n
+        return summ
 
 class GeometricProgression(Progression):
-  def __init__(self, first, step):
-    super().__init__(first, step)
+    def __init__(self, first, step):
+        super().__init__(first, step)
 
-  def compute_n_elem(self, n):
-    a_n = self.first
-    for i in range(n - 1):
-      a_n *= self.step
-    return a_n
+    def compute_n_elem(self, n):
+        a_n = self.first
+        for i in range(n - 1):
+            a_n *= self.step
+        return a_n
 
-  def sum(self, n):
-    summ = 0
-    for i in range(1, n + 1):
-      a_n = self.first * self.step ** (i - 1)
-      summ += a_n
-    return summ
+    def sum(self, n):
+        summ = 0
+        for i in range(1, n + 1):
+            a_n = self.first * self.step ** (i - 1)
+        summ += a_n
+        return summ
 
-'''
-obj1 = ArithmeticProgression(1, 10)
-print(obj1.compute_n_elem(10))
-print(obj1.sum(10))
+progr = Progression(1, 2)
 
-obj1 = GeometricProgression(1, 10)
-print(obj1.compute_n_elem(10))
-print(obj1.sum(10))
-'''
-choose = input()
-first, step = list(map(int, input().split()))
-count = int(input())
-if (choose == 'arithmetic'):
-    obj1 = ArithmeticProgression(first, step)
-    print(obj1.compute_n_elem(count))
-    print(obj1.sum(count))
-elif (choose == 'geometric'):
-    obj1 = GeometricProgression(first, step)
-    print(obj1.compute_n_elem(count))
-    print(obj1.sum(count))
-else:
-    print('Cant create this progression')
+progr.set_strategy(ArithmeticProgression(1, 5))
+progr.compute_n_elem(3)
+progr.sum(4)
+
+progr.set_strategy(GeometricProgression(1, 2))
+progr.compute_n_elem(3)
+progr.sum(4)
